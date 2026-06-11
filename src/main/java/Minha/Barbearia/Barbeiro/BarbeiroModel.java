b97b8034fc43daa91d96053e7a8204816296fa85
Kaua033 <suelen.kaua16@gmail.com>
2026-06-10 23:41:17 -0300

    reafactor:BarbeiroModel

--- src/main/java/Minha/Barbearia/Barbeiro/BarbeiroModel.java
+++ src/main/java/Minha/Barbearia/Barbeiro/BarbeiroModel.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class BarbeiroModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
 
